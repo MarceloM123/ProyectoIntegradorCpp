aca53b11228275e33912c54eaf3a5b27cfc8ab2f
MarceloM123 <58484864+MarceloM123@users.noreply.github.com>
2020-06-11 21:52:55 -0500

    Add files via upload

---
+++ Proyecto Integrador/Proyecto Integrador/Peliculas.h
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#ifndef Peliculas_h
+#define Peliculas_h
+
+#include "Video.h"
+
+class Peliculas : public Video {
+private:
+    string duracion;
+    double calificacion;
+public:
+    Peliculas();
+    Peliculas(int ID, string nombre, string duracion, string genero, double calificacion);
+    //void setDuracion(string duracion);
+    //void setCalificacion(double calificacion);
+
+    string getDuracion();
+    double getCalificacion();
+
+    void show();
+};
+
+Peliculas::Peliculas():Video() {
+    duracion = "00:00";
+    calificacion = 0.0;
+}
+
+Peliculas::Peliculas(int ID, string nombre, string duracion, string genero, double calificacion):Video(ID, nombre, genero) {
+    this -> duracion = duracion;
+    this -> calificacion = calificacion;
+}
+
+/*void Peliculas::setDuracion(string duracion) {
+    this -> duracion = duracion;
+}
+
+void Peliculas::setCalificacion(double calificacion) {
+    this -> calificacion = calificacion;
+}*/
+
+string Peliculas::getDuracion() {
+    return duracion;
+}
+
+double Peliculas::getCalificacion() {
+    return calificacion;
+}
+
+void Peliculas::show() {
+    Video::show();
+    cout << "Duración: " << duracion << ". Calificación: " << calificacion << "." << endl;
+}
+
+#endif
---
+++ Proyecto Integrador/Proyecto Integrador/main.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+#include "Video.h"
+#include "Peliculas.h"
+#include "Series.h"
+#include "Episodios.h"
+
+string cargarDatos(vector<Peliculas*> peliculas, vector<Series*> series, vector<Episodios*> episodios) {
+    ifstream epi("ProyectoIntegrador-Episodios-1.csv");
+    ifstream peli("ProyectoIntegrador-Peliculas-1.csv");
+    ifstream seri("ProyectoIntegrador-Series-1.csv");
+
+    string line;
+    string valor;
+    char delim = ',';
+
+    string nombre, duracion, genero, IDEpisodio;
+    int ID, numDeTemporadas, temporada;
+    double calificacion;
+
+    int col;
+
+    if(epi.is_open()) {
+        getline(epi, line);
+        while(getline(epi, line)) {
+
+            stringstream ss(line);
+            col = 0;
+
+            while(getline(ss, valor, delim)) {
+
+                switch(col) {
+                case 0:
+                    ID = stoi(valor);
+                    break;
+                case 1:
+                    nombre = valor;
+                    break;
+                case 2:
+                    duracion = valor;
+                    break;
+                case 3:
+                    genero = valor;
+                    break;
+                case 4:
+                    calificacion = stod(valor);
+                    break;
+                }
+                col++;
+            }
+            peliculas.push_back(new Peliculas(ID, nombre, duracion, genero, calificacion));
+        }
+        peli.clos();
+    }
+
+
+
+
+}
+
+
+
+
+
+
+int main() {
+    vector<Peliculas*> peliculas;
+    vector<Series*> series;
+    vector<Episodios*> episodios; 
+
+    int opc;
+    cout << endl << "-------------------------------------------------------------------------------------------" << endl;
+    cout << "1) Cargar archivos de datos." << endl;
+    cout << "2) Mostrar los videos en general con un cierto rango de calificación de un cierto género." << endl;
+    cout << "3) Mostrar los videos en general de un cierto género." << endl;
+    cout << "4) Mostrar los episodios de una determinada serie con un rango de calificación determinada." << endl;
+    cout << "5) Mostrar las películas con cierto rango de calificación." << endl;
+    cout << "6) Calificar un video." << endl;
+    cout << "0) Salir." << endl;
+    cout << "-------------------------------------------------------------------------------------------" << endl;
+    cout << "Opcion: ";
+    cin >> opc;
+
+    bool sw = true;
+    int opcCase1;
+
+    while (sw) {
+
+        switch (opc) {
+
+        case 1:
+            // Cargar archivos de datos
+            break;
+
+        case 2:
+            // Mostrar los videos en general con un cierto rango de calificación de un cierto género
+            break;
+
+        case 3:
+            // Mostrar los videos en general de un cierto género
+            break;
+
+        case 4:
+            // Mostrar los episodios de una determinada serie con un rango de calificación determinada
+            break;
+
+        case 5:
+            // Mostrar las películas con cierto rango de calificación
+            break;
+
+        case 6:
+            // Calificar un video
+            break;
+
+        case 0:
+            // Salir
+            sw = false;
+            break;
+
+        }
+    }
+    return 0;
+}
